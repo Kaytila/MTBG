1b9d8d54e1783cbd5f355048a42246d4531a721c
kaytila <claus.kick@gmail.com>
2022-12-17 09:13:44 +0100

    2022-12-17: play with git a bit.

--- src/net/ck/game/ui/EQPanel.java
+++ src/net/ck/game/ui/EQPanel.java
@@ -1,13 +1,30 @@
 package net.ck.game.ui;
 
+import net.ck.game.items.ArmorPositions;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class EQPanel extends JComponent
+public class EQPanel extends JComponent implements EQPanelArmorType
 {
+
+    private ArmorPositions armorPosition;
+
     public void paintComponent(Graphics g)
     {
         g.setColor(Color.black);
         g.drawOval(0,0, 50, 50);
+    }
+
+    @Override
+    public ArmorPositions getArmorPosition()
+    {
+        return armorPosition;
+    }
+
+    @Override
+    public void setArmorPosition(ArmorPositions position)
+    {
+        armorPosition = position;
     }
 }
---
+++ src/net/ck/game/ui/EQPanelArmorType.java
@@ -0,0 +1,11 @@
+package net.ck.game.ui;
+
+import net.ck.game.items.ArmorPositions;
+
+public interface EQPanelArmorType
+{
+
+    ArmorPositions getArmorPosition();
+
+    void setArmorPosition(ArmorPositions position);
+}
