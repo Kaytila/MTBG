3ba921a0bf04544482c6f43e808813a9cca8cb7e
kaytila <claus.kick@gmail.com>
2023-03-21 08:55:13 +0100

    updated gradle

--- gradle/build-logic/build.gradle.kts
+++ gradle/build-logic/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(16))
         vendor.set(JvmVendorSpec.ADOPTIUM)
     }
 }
--- gradle/build-logic/settings.gradle.kts
+++ gradle/build-logic/settings.gradle.kts
@@ -1,1 +1,4 @@
 rootProject.name = "build-logic"
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.4.0")
+}
